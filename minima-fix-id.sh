b9bd11e53217baf0f2422b827d1afcd44708c64e
isafin509 <70804056+isafin509@users.noreply.github.com>
2022-08-04 19:47:49 +0300

    added port var

--- minima-fix-id.sh
+++ minima-fix-id.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 nid="YOUR_NODE_ID"
-
+port=9002
 while :
 do
     sleep 30
-    tid=$(curl 127.0.0.1:9002/incentivecash | jq -r '.response.uid')
+    tid=$(curl 127.0.0.1:{$port}/incentivecash | jq -r '.response.uid')
     echo "Current id: $tid"
     echo "Your id: $nid"
     if [[ "$nid" != "$tid" ]]; then
         echo "Missmatch, Fixing..."
-        curl 127.0.0.1:9002/incentivecash%20uid:$nid
+        curl 127.0.0.1:{$port}/incentivecash%20uid:$nid
         echo "  Fixed!"
     fi
 done
